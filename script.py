36a90b5e7a3ea0774e21a558902fd4bdd01690b3
Pau Camós Parra <pau.camos@apartool.com>
2020-12-21 23:25:32 +0100

    New version

---
+++ script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import sqlite3
+import re
+
+# function
+def deleteHistory():
+    count = 0
+    result = True
+    id = 0
+    while result:
+        result = False
+        ids = []
+        for row in c.execute("""SELECT id, url FROM urls WHERE url LIKE '%{}%'""".format(prompt)):
+            print (row)
+            id = row[0]
+            ids.append((id,))
+            count += 1
+        c.executemany('DELETE FROM urls WHERE id=?', ids)
+        conn.commit()
+
+    conn.close()
+    return count
+
+# connect to the database
+conn = sqlite3.connect('c:/Users/Pablo/AppData/Local/Google/Chrome/User Data/Default/History')
+c = conn.cursor()
+
+# history count
+print("history length", c.execute('SELECT count(1) FROM urls').fetchone()[0])
+
+# user input
+prompt = input("What do you want to remove? ")
+confirmation = input("Are you sure you want to remove {} (y/n)".format(prompt))
+
+
+if (confirmation == "y"):
+    elements = deleteHistory()
+    print("{} elements have been removed".format(elements))
+else:
+    print("No records have been removed.")
